398320f01244fa7abe36292eeeb5f9fee8254b7d
A-Kiombo <anibalkiombo0@gmail.com>
2026-03-13 12:21:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'hinario-v1';
+const ASSETS = [
+  './hinario.html',
+  './manifest.json'
+];
+
+// Instala o Service Worker e guarda os ficheiros no cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Responde com o cache quando estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
